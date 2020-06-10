b3ada32fac1fd052411d437f095b1fd921b9287c
atemmel <temmeladam@gmail.com>
2020-06-10 22:00:49 +0000

    Did some moreoreoreoroeroeo

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"image/color"
 	"encoding/json"
 	"log"
+	"os"
 )
 
 var tileset *ebiten.Image
 var p1img []*ebiten.Image
+var playerImg *ebiten.Image
 var dot *ebiten.Image
 var dotX float64
 var dotY float64
@@ -27,11 +29,17 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	p1img = append(p1img, img)
+
+	playerImg, _, err = ebitenutil.NewImageFromFile("./resources/images/lcuas.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	tileset, _, err = ebitenutil.NewImageFromFile("./resources/images/tileset1.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
-	p1img = append(p1img, img)
 
 	dot, err = ebiten.NewImage(32, 32, ebiten.FilterDefault)
 	if err != nil {
@@ -53,12 +61,80 @@ func init() {
 
 type Game struct{
 	tileMap TileMap
+	path string
+	player Player
 }
 
 type TileMap struct {
 	Width int
 	Height int
 	Tiles []int
+}
+
+const framesPerState = 3
+const playerMaxCycle = 4
+const playerVelocity = (tileSize / 2) / playerMaxCycle
+
+type Player struct {
+	gx float64
+	gy float64
+	x int
+	y int
+	state int
+	frames int
+	tx int
+	ty int
+	dir Direction
+}
+
+type Direction int
+
+const(
+	Static Direction = 0
+	Down Direction = 1
+	Left Direction = 2
+	Right Direction = 3
+	Up Direction = 4
+)
+
+func (player *Player) Step(dir Direction) {
+	if player.dir == Static {
+		player.dir = dir
+	} else {
+		player.frames++
+
+		if player.frames == framesPerState {
+			player.frames = 0
+			if player.dir == Up {
+				player.ty = 34
+				player.gy += -playerVelocity
+			} else if player.dir == Down {
+				player.ty = 0
+				player.gy += playerVelocity
+			} else if player.dir == Left {
+				player.ty = 34 * 2
+				player.gx += -playerVelocity
+			} else if player.dir == Right {
+				player.ty = 34 * 3
+				player.gx += playerVelocity
+			}
+
+			if player.state % 2 == 0 {
+				player.NextAnim()
+			}
+			player.state++
+			if player.state == playerMaxCycle {
+				player.state = 0
+				player.dir = Static
+				player.tx = 0
+			}
+		}
+	}
+
+}
+
+func (player *Player) NextAnim() {
+	player.tx += 34
 }
 
 var scroll = 0.
@@ -84,14 +160,39 @@ func (g *Game) Update(screen *ebiten.Image) error {
 		fmt.Println("selectedTile:", selectedTile)
 	}
 
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		g.Save()
+		os.Exit(0)
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.player.Step(Up)
+	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.player.Step(Down)
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.player.Step(Right)
+	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.player.Step(Left)
+	} else {
+		g.player.Step(Static)
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.DrawTileset(screen)
+
+	playerOpt := &ebiten.DrawImageOptions{}
+	playerOpt.GeoM.Translate(g.player.gx + 7, g.player.gy + 1)
+	playerOpt.GeoM.Scale(2,2)
+	screen.DrawImage(playerImg.SubImage(image.Rect(g.player.tx, g.player.ty, g.player.tx + tileSize, g.player.ty + tileSize)).(*ebiten.Image), playerOpt)
+
+	/*
 	for _, img := range p1img {
 		screen.DrawImage(img, nil)
 	}
+	*/
 
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(dotX, dotY);
@@ -107,7 +208,15 @@ func (g *Game) Load(str string) {
 	if err != nil {
 		panic(err)
 	}
+	g.path = str
+}
 
+func (g *Game) Save() {
+	bytes, err := json.Marshal(g.tileMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ioutil.WriteFile(g.path, bytes, 0644)
 }
 
 func (g *Game) DrawTileset(screen *ebiten.Image) {
